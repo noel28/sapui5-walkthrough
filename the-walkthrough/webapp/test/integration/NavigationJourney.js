93ae2be3ce8e0e81f336050961120b70f41f1231
noel28 <43724953+noel28@users.noreply.github.com>
2020-01-23 18:08:53 +0800

    Commit OPA - QUnit Test

---
+++ the-walkthrough/webapp/test/integration/NavigationJourney.js
@@ -0,0 +1,33 @@
+/* global QUnit, opaTest */
+
+sap.ui.define([
+	"Walkthrough/localService/mockserver",
+	"sap/ui/test/opaQunit",
+	"./pages/App"
+],	function(mockserver) {
+	"user strict";
+	
+	QUnit.module("Navigation");
+	
+	opaTest("Should open the Hello Dialog", function(Given, When, Then) {
+		// initialize the mock server
+		mockserver.init();
+		
+		// Arrangements
+		Given.iStartMyUIComponent({
+			componentConfig: {
+				name: "Walkthrough"
+			}
+		});
+		
+		// Actions
+		When.onTheAppPage.iPressTheSayHelloWithDialogButton();
+		
+		// Assertions
+		Then.OnTheAppPage.iShouldSeeTheHelloDialog();
+		
+		// Cleanup
+		Then.iTeardownMyApp();
+		
+	});
+});
---
+++ the-walkthrough/webapp/test/integration/pages/App.js
@@ -0,0 +1,37 @@
+sap.ui.define([
+	"sap/ui/test/Opa5",
+	"sap/ui/test/actions/Press"
+], function(Opa5, Press) {
+	"use strict";
+	
+	var sViewName = "Walkthrough.view.HelloPanel";
+	
+	Opa5.createPageObjects({
+		onTheAppPage: {
+			actions: {
+				iPressTheSayHelloWithDialogButton: function() {
+					return this.waitFor({
+						id: "helloDialogButton",
+						viewName: sViewName,
+						actions: new Press(),
+						errorMessage: "Did not find the 'Say Hello With Dialog' button on the Hello Panel View"
+					});
+				}
+			},
+			
+			assertions:	{
+				iShouldSeeTheHelloDialog:	function() {
+					return this.waitFor({
+						controllType: "sap.m.dialog",
+						success: function() {
+							// we set the view busy, so we need to query the parent of the app
+							Opa5.assert.ok(true, "The dialog is open");
+						},
+
+						errorMessage: "Did not find teh dialog control"
+					});
+				}
+			}
+		}
+	});
+});
